33087ef0b0fd8b443ea3d8c6643a0b5041816a02
Iv077 <iv077@live.mdx.ac.uk>
2022-02-19 18:44:50 +0000

    APPLAYING IDEA

--- Codes/something.py
+++ Codes/something.py
@@ -101,535 +101,125 @@ while True:
         client.put_pixels(leds)
         time.sleep(.1)
     time.sleep(1)
-    
-    leds = [(0,0,0)]*360                #P
-    rand_color = (random.randint(rand_color[0]-50, rand_color[0]+50),random.randint(rand_color[1]-50, rand_color[1]+50),random.randint(rand_color[2]-20, rand_color[2]+20))
-    leds[309-led] = rand_color
-    leds[249-led] = rand_color
-    leds[189-led] = rand_color
-    leds[189-led+4] = rand_color
-    leds[189-led+2] = rand_color
-    leds[129-led] = rand_color
-    leds[129-led+6] = rand_color
-    leds[69-led] = rand_color
-    leds[69-led+2] = rand_color
-    leds[69-led+4] = rand_color
-
-    leds[318-led] = rand_color          #L
-    leds[318-led+2] = rand_color
-    leds[318-led+4] = rand_color
-    leds[318-led+6] = rand_color
-    leds[258-led] = rand_color
-    leds[198-led] = rand_color
-    leds[138-led] = rand_color
-    leds[78-led] = rand_color
-
-    leds[325-led] = rand_color           #A  
-    leds[325-led+8] = rand_color
-    leds[266-led] = rand_color
-    leds[266-led+6] = rand_color
-    leds[207-led] = rand_color
-    leds[207-led+2] = rand_color
-    leds[207-led+4] = rand_color
-    leds[148-led] = rand_color
-    leds[148-led+2] = rand_color
-    leds[89-led] = rand_color
-
-    leds[334-led+3] = rand_color        #Y
-    leds[274-led+3] = rand_color
-    leds[214-led+3] = rand_color
-    leds[154-led+2] = rand_color
-    leds[154-led+4] = rand_color
-    leds[94-led] = rand_color
-    leds[94-led+6] = rand_color
-
-    leds[343-led] = rand_color          #E
-    leds[343-led+2] = rand_color
-    leds[343-led+4] = rand_color
-    leds[343-led+6] = rand_color
-    leds[283-led] = rand_color
-    leds[223-led] = rand_color
-    leds[223-led+4] = rand_color
-    leds[223-led+2] = rand_color
-    leds[163-led] = rand_color
-    leds[103-led] = rand_color
-    leds[103-led+2] = rand_color
-    leds[103-led+4] = rand_color
-    leds[103-led+6] = rand_color
-
-    leds[351-led] = rand_color       #R
-    leds[351-led+6] = rand_color
-    leds[291-led] = rand_color
-    leds[291-led+4] = rand_color
-    leds[231-led] = rand_color
-    leds[231-led+4] = rand_color
-    leds[231-led+2] = rand_color
-    leds[171-led] = rand_color
-    leds[171-led+6] = rand_color
-    leds[111-led] = rand_color
-    leds[111-led+2] = rand_color
-    leds[111-led+4] = rand_color
-    client.put_pixels(leds)
-    time.sleep(.1)
-time.sleep(1)
-
-
-introduction = input('''Welcome
-                \n Insert your name
-                ''') # \n-newline; \t- tab
+    break
 
 led = 0
-def funcA():
-    leds = [(0,0,0)]*360      #todo de un color
-    leds[351-led] = (255,0,0)
-    leds[351-led+8] = (255,0,0)
-    leds[292-led] = (255,0,0)
-    leds[292-led+6] = (255,0,0)
-    leds[233-led] = (255,0,0)
-    leds[233-led+2] = (255,0,0)
-    leds[233-led+4] = (255,0,0)
-    leds[174-led] = (255,0,0)
-    leds[174-led+2] = (255,0,0)
-    leds[115-led] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcB():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-    return(introduction)
-def funcC():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
+while led<60:
+    for led in range(60):  
+        leds = [(0,0,0)]*360                #P
+        rand_color = (random.randint(rand_color[0]-50, rand_color[0]+50),random.randint(rand_color[1]-50, rand_color[1]+50),random.randint(rand_color[2]-20, rand_color[2]+20))
+        leds[309-led] = rand_color
+        leds[249-led] = rand_color
+        leds[189-led] = rand_color
+        leds[189-led+4] = rand_color
+        leds[189-led+2] = rand_color
+        leds[129-led] = rand_color
+        leds[129-led+6] = rand_color
+        leds[69-led] = rand_color
+        leds[69-led+2] = rand_color
+        leds[69-led+4] = rand_color
 
-def funcD():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcE():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcF():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcG():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[231-led+5] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+3] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+5] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcH():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcI():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+3] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led+3] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led+3] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+3] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcJ():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[171-led+4] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcK():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+5] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+5] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcL():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcM():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+2] = (255,0,0)
-    leds[171-led+4] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcN():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+5] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+2] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+1] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
+        leds[318-led] = rand_color          #L
+        leds[318-led+2] = rand_color
+        leds[318-led+4] = rand_color
+        leds[318-led+6] = rand_color
+        leds[258-led] = rand_color
+        leds[198-led] = rand_color
+        leds[138-led] = rand_color
+        leds[78-led] = rand_color
 
-def funcO():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcP():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcQ():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcR():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcS():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led+2] = (255,0,0)
-    leds[231-led+4] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcT():
-    leds = [(0,0,0)]*360
-    leds[351-led+3] = (255,0,0)
-    leds[291-led+3] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led+3] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+3] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcU():
-    leds = [(0,0,0)]*360
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcV():
-    leds = [(0,0,0)]*360
-    leds[351-led+3] = (255,0,0)
-    leds[291-led+2] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[231-led+1] = (255,0,0)
-    leds[231-led+5] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcW():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led] = (255,0,0)
-    leds[291-led+2] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[231-led+6] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+6] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcX():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led+2] = (255,0,0)
-    leds[291-led+4] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led+2] = (255,0,0)
-    leds[171-led+4] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcY():
-    leds = [(0,0,0)]*360
-    leds[351-led+3] = (255,0,0)
-    leds[291-led+3] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led+2] = (255,0,0)
-    leds[171-led+4] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-def funcZ():
-    leds = [(0,0,0)]*360
-    leds[351-led] = (255,0,0)
-    leds[351-led+2] = (255,0,0)
-    leds[351-led+4] = (255,0,0)
-    leds[351-led+6] = (255,0,0)
-    leds[291-led+2] = (255,0,0)
-    leds[291-led+6] = (255,0,0)
-    leds[231-led+3] = (255,0,0)
-    leds[171-led] = (255,0,0)
-    leds[171-led+4] = (255,0,0)
-    leds[111-led] = (255,0,0)
-    leds[111-led+2] = (255,0,0)
-    leds[111-led+4] = (255,0,0)
-    leds[111-led+6] = (255,0,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
+        leds[325-led] = rand_color           #A  
+        leds[325-led+8] = rand_color
+        leds[266-led] = rand_color
+        leds[266-led+6] = rand_color
+        leds[207-led] = rand_color
+        leds[207-led+2] = rand_color
+        leds[207-led+4] = rand_color
+        leds[148-led] = rand_color
+        leds[148-led+2] = rand_color
+        leds[89-led] = rand_color
 
+        leds[334-led+3] = rand_color        #Y
+        leds[274-led+3] = rand_color
+        leds[214-led+3] = rand_color
+        leds[154-led+2] = rand_color
+        leds[154-led+4] = rand_color
+        leds[94-led] = rand_color
+        leds[94-led+6] = rand_color
 
+        leds[343-led] = rand_color          #E
+        leds[343-led+2] = rand_color
+        leds[343-led+4] = rand_color
+        leds[343-led+6] = rand_color
+        leds[283-led] = rand_color
+        leds[223-led] = rand_color
+        leds[223-led+4] = rand_color
+        leds[223-led+2] = rand_color
+        leds[163-led] = rand_color
+        leds[103-led] = rand_color
+        leds[103-led+2] = rand_color
+        leds[103-led+4] = rand_color
+        leds[103-led+6] = rand_color
 
+        leds[351-led] = rand_color       #R
+        leds[351-led+6] = rand_color
+        leds[291-led] = rand_color
+        leds[291-led+4] = rand_color
+        leds[231-led] = rand_color
+        leds[231-led+4] = rand_color
+        leds[231-led+2] = rand_color
+        leds[171-led] = rand_color
+        leds[171-led+6] = rand_color
+        leds[111-led] = rand_color
+        leds[111-led+2] = rand_color
+        leds[111-led+4] = rand_color
+        client.put_pixels(leds)
+        time.sleep(.1)
+    time.sleep(1)
+    break
 
-##while True:
-##    if introduction.isalpha() == True:
-##        
-##    else:
-##        introduction=input("Don't try with numbers, select letters") # ask for new value
+value = input('''FROM THIS MOMENT YOU ARE GOING TO PLAY AGAINST 'THE PANEL' TO
+                    \n AN INCREDIBLE DANGEROUS GAME:
+              
+                    \n NOW CHOOSE YOUR WEAPON:
 
-while led<60:                 #hacer el movimiento para siempre 
-    for led in range(52):
-        if introduction == 'A':
-            funcA()
-        elif introduction == 'B':
-            print(funcB())
-        elif introduction == 'C':
-            print(funcC())
-        elif introduction == 'D':
-            print(funcD())
-        elif introduction == 'E':
-            print(funcE())
-        elif introduction == 'F':
-            print(funcF())
-        elif introduction == 'G':
-            print(funcG())
-        elif introduction == 'H':
-            print(funcH())
-        elif introduction == 'I':
-            print(funcI())
-        elif introduction == 'J':
-            print(funcJ())
-        elif introduction == 'K':
-            print(funcK())
-        elif introduction == 'L':
-            print(funcL())
-        elif introduction == 'M':
-            print(funcM())
-        elif introduction == 'N':
-            print(funcN())
-        elif introduction == 'O':
-            print(funcO())
-        elif introduction == 'P':
-            print(funcP())
-        elif introduction == 'Q':
-            print(funcQ())
-        elif introduction == 'R':
-            print(funcR())
-        elif introduction == 'S':
-            print(funcS())
-        elif introduction == 'T':
-            print(funcT())
-        elif introduction == 'U':
-            print(funcU())
-        elif introduction == 'V':
-            print(funcV())
-        elif introduction == 'W':
-            print(funcW())
-        elif introduction == 'X':
-            print(funcX())
-        elif introduction == 'Y':
-            print(funcY())
-        elif introduction == 'Z':
-            print(funcZ())
+                    \n 1. ROCK
+                    \n 2. PAPER
+                    \n 3. SCISSORS
+                    \n 4. LIZARD
+                    \n 5. SPOKE 
+                ''') # \n-newline; \t- tab
+
+def func1(val):
+    return val**val;
+def func2(val):
+    return val**val;
+def func3(val):
+    return val**val;
+def func4(val):
+    return val**val;
+def func5(val):
+    return val**val;
+
+while True:
+    if value.isdigit() == True: #.isgigit()
+        value = int(value)
+        if value > 5 or value < 1: #if value is outside our range:
+            value = input("That option is not possible, try a number between 1 and 5")
+            continue #skip rest of loop, start from isdigit() check again.
+        else:
+            break # on correct value datatype: exit the loop
+    else:
+        value=input("Don't try with letters, select a number:") # ask for new value
+
+if value == 1:
+    print(func1(value))
+elif value == 2:
+    print(func2(value))
+elif value == 3:
+    print(func3(value))
+elif value == 4:
+    print(func4(value))
+elif value == 5:
+    print(func5(value))
 
